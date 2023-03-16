40fc4a3bedb07ea98d786074a77659fa0c55ba6f
jsto890 <127268851+jsto890@users.noreply.github.com>
2023-03-17 08:47:43 +1300

    Add files via upload

---
+++ module_lab2_task1.py
@@ -0,0 +1,12 @@
+import numpy as np
+# additional imports here
+
+
+class LUSolver(object):
+    def __init__(self):
+        self.matrix_a = None
+        self.matrix_l = None
+        self.matrix_u = None
+        self.vector_b = None
+        self.vector_x = None
+        self.vector_y = None
